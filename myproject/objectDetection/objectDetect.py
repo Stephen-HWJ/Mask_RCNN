df550968c365ff3bddb17411adb8e81e22f30fa9
Weiji Huang <hwj55801439@gmail.com>
2019-11-19 17:48:02 +0000

    baseline completed

--- myproject/objectDetection/objectDetect.py
+++ myproject/objectDetection/objectDetect.py
@@ -105,7 +105,7 @@ class ObjectDetectionConfig(Config):
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
-    IMAGES_PER_GPU = 2
+    IMAGES_PER_GPU = 1
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 5  # Background + building-ground classes
