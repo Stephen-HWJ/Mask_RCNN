6ef90f0068c4d9731c46371380a8b7d074a36042
Weiji Huang <weijihua@usc.edu>
2019-11-25 22:24:58 -0800

    dataset updated

--- myproject/objectDetection/objectsDatasets/training/images/new/untitled.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import os
-labelNames = [x for x in os.listdir('.') if ".jpg" in x] #new
-for name in labelNames:
-	img = cv2.imread(name)
-	img_new = cv2.resize(img, (400, 300))
-	cv2.imwrite('../'+name, img_new)
