65f0109573c0413e9d36e647c9dbbe7daf2cb8c7
Weiji Huang <weijihua@usc.edu>
2019-11-28 17:51:13 -0800

    change model debug

--- mrcnn/model_my.py
+++ mrcnn/model_my.py
@@ -179,7 +179,7 @@ def resnet_graph(input_image, architecture, stage5=False, train_bn=True):
         return x[..., :3]
 
     # A1, A2, A3, A4, A5 = resnet_graph_rgb(input_image[..., :3], architecture, stage5, train_bn)
-    A1, A2, A3, A4, A5 = resnet_graph_rgb(Lambda(slice)(input_image), architecture, stage5, train_bn)
+    A1, A2, A3, A4, A5 = resnet_graph_rgb(KL.Lambda(slice)(input_image), architecture, stage5, train_bn)
     print(A1.shape, A2.shape, A3.shape, A4.shape, A5.shape)
 
     t_in = tf.convert_to_tensor(input_image[..., 3])
