d761addc40fd3826f74522415c490321a488b6f1
Weiji Huang <weijihua@usc.edu>
2019-11-28 16:39:47 -0800

    change model debug

--- mrcnn/model_my.py
+++ mrcnn/model_my.py
@@ -181,7 +181,7 @@ def resnet_graph(input_image, architecture, stage5=False, train_bn=True):
     t_in = tf.reshape(t_in, [-1, -1, -1, 1])
     print(t_in.shape)
     B1, B2, B3, B4, B5 = resnet_graph_thermal(t_in, architecture, stage5, train_bn)
-    return [KL.Concatenate(A1, B1), KL.Concatenate(A2, B2), KL.Concatenate(A3, B3), KL.Concatenate(A4, B4), KL.Concatenate(A5, B5)]
+    return [KL.Concatenate([A1, B1]), KL.Concatenate([A2, B2]), KL.Concatenate([A3, B3]), KL.Concatenate([A4, B4]), KL.Concatenate([A5, B5])]
 
 
 
